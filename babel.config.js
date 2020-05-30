8eab4d4359e838103284e202b47f7526698185ed
Renée Kooi <renee@kooi.me>
2020-05-30 19:33:05 +0200

    rewrite acorn-private-class-elements import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+function rewritePrivateClassElementImport (babel) {
+  return {
+    visitor: {
+      CallExpression: function (path) {
+        if (path.get('callee').isIdentifier({ name: 'require' }) &&
+          path.get('arguments.0').isLiteral({ value: 'acorn-private-class-elements' })) {
+          path.get('arguments.0').node.value = '../acorn-private-class-elements'
+        }
+      }
+    }
+  }
+}
+
 module.exports = {
   presets: [
     ['@babel/preset-env', {
@@ -5,5 +18,6 @@ module.exports = {
     }]
   ],
   plugins: [
+    rewritePrivateClassElementImport
   ]
 }
