30f64c2dd503313260a793702f65a7e17bb87910
Mark Stacey <markjstacey@gmail.com>
2021-10-22 20:46:42 -0230

    Update `acorn` and increase default `ecmaVersion`
    `acorn` has been updated to v8.5, and `acorn-walk` has been updated to
    v8.2. These versions include many new bug fixes and features, including
    the features currently being added by this package using plugins. These
    features are supported by `acorn` only when `ecmaVersion` is set to
    "2022", so the default `ecmaVersion` has been set to `2022`.
    
    Since the plugins are no longer needed, they have been removed, along
    with all related build steps and documentation.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 var acorn = require('acorn')
 
 var CJSParser = acorn.Parser
-  // Required paths should be relative to the package root, because Babel outputs this file there.
-  .extend(require('./lib/acorn-class-fields'))
-  .extend(require('./lib/acorn-static-class-features'))
   .extend(defaultOptionsPlugin)
 var ESModulesParser = CJSParser
 
 function mapOptions (opts) {
   if (!opts) opts = {}
   return {
-    ecmaVersion: 2021,
+    ecmaVersion: 2022,
     allowHashBang: true,
     allowReturnOutsideFunction: true,
     ...opts
